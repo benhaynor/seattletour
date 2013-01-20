7ea21d7efd5f85a4e0379af009f24761d9d64891
benhaynor <ben@meltwater.org>
2013-01-20 16:27:20 +0000

    its cool

--- makemap.js
+++ makemap.js
@@ -5,33 +5,71 @@
 var map;
 var tourGuide;
 
-function Destination(latLng, zoomLevel, name){
+function Destination(latLng, zoomLevel,pictures){
 	this.type = "Destination";
 	this.latLng = latLng;
 	this.zoomLevel = zoomLevel;
 	this.name = name;
+	this.pictures = pictures || [];
+	this.currentPicture = -1;
 }
 
 Destination.prototype.action = function(){
-	map.setZoom(this.zoomLevel);
-	map.panTo(this.latLng);
-	window.console.log(this.name);
+	if (this.currentPicture == -1){
+  	map.setZoom(this.zoomLevel);
+  	map.panTo(this.latLng);
+	} else if (this.currentPicture <= this.pictures.length) {
+		window.console.log("got me", this.currentPicture);
+		var cp = this.pictures[this.currentPicture];
+		cp.action();
+	}
+	this.currentPicture ++;
+	if (this.currentPicture >= this.pictures.length){
+		window.console.log("done");
+		return "done";	
+	} else{
+		window.console.log("not done");
+		return "not done";
+	}
 }
 
-var seattleCenterLatLng = new google.maps.LatLng(47.641141,-122.35382);
+function imageEvents(imageList,srcFolder){
+	var imageEventList = [];
+	var openner = new ModalEvent(new modal($('<img align="middle" src="' + srcFolder + '/' +imageList[0]+ '">')));
+	imageEventList.push(openner);
+	for (var i=1; i < imageList.length; i ++){
+		imageEventList.push(new NextImageEvent(
+					$('<img src="' + srcFolder + '/' + imageList[i] + '">'),
+					openner.modal));
+	}
+	imageEventList.push(openner);
+	return imageEventList;
+}
+
+var seattleCenterLatLng = new google.maps.LatLng(47.62049,-122.349649);
 var usaCenterView = new Destination(new google.maps.LatLng(38.891033,-94.833984),4);
 var waCenterView = new Destination(seattleCenterLatLng,7);
 var seattleCenterView = new Destination(seattleCenterLatLng,9);
-var olympicCenterView = new Destination(new google.maps.LatLng(47.709762,-123.475342),9);
-var cascadesCenterView = new Destination(new google.maps.LatLng(47.805776,-121.069336),9);
-var warrenAveN = new Destination(new google.maps.LatLng(47.641141,-122.35382), 14);
-var spaceNeedle = new Destination(new google.maps.LatLng(47.62049,-122.349649),14);
-var pikePlaceMarket = new Destination(new google.maps.LatLng(47.610136,-122.342057),14);
+var olympicPictures = imageEvents(["1olympics.jpg","2olympics.jpg","3olympics.jpg"],"imgs/olympics");
+var olympicCenterView = new Destination(new google.maps.LatLng(47.709762,-123.475342),9, olympicPictures);
+var cascadesPictures = imageEvents(["1cascades.jpg","2cascades.jpg","3cascades.jpg"],"imgs/cascades");
+var cascadesCenterView = new Destination(new google.maps.LatLng(47.805776,-121.069336),9, cascadesPictures);
+var seattleCenterView2 = new Destination(seattleCenterLatLng,9);
+var imgList = imageEvents(["bumbershoot.jpg","emp.jpg","foklife.jpg","spaceNeedle.jpg"],"imgs/seattleCenter");
+var spaceNeedle = new Destination(seattleCenterLatLng,14,imgList);
+var pikePlacePictures = imageEvents(['pikeplace1.jpg','pikeplace2.jpg','pikeplace3.jpg'],'imgs/pikeplace');
+var pikePlaceMarket = new Destination(new google.maps.LatLng(47.610136,-122.342057),14, pikePlacePictures);
+var garfieldHigh = new Destination(new google.maps.LatLng(47.605334,-122.301807),14);
+var universityOfWashington = new Destination(new google.maps.LatLng(47.65601,-122.30934),14);
+var ballardLocks = new Destination(new google.maps.LatLng(47.666705,-122.398068),14);
+var sayHiEvent = new ModalEvent(modal($('<img src="imgs/hi.jpg">"')));
+var nextImage = new NextImageEvent($("<p>Hey!</p>"),sayHiEvent.modal);
+var discoverParkLighthouse = new Destination(new google.maps.LatLng(47.661977,-122.435782),14);//[sayHiEvent,nextImage,sayHiEvent]);
 
-var sayHiEvent = new ModalEvent(modal($("<p>Hi</p>")));
 var events = [usaCenterView, waCenterView,seattleCenterView,olympicCenterView,
-		cascadesCenterView,seattleCenterView, warrenAveN,
-		spaceNeedle, pikePlaceMarket];
+		cascadesCenterView, seattleCenterView2, 
+		spaceNeedle, pikePlaceMarket, garfieldHigh, universityOfWashington,
+		ballardLocks, discoverParkLighthouse];
 
 /*
  * Creates a modal object with 
@@ -48,6 +86,16 @@ function modal(bodyContent){
 	}).append(modalBody);
 }
 
+function NextImageEvent(bodyNode,modal){
+	this.modal = modal;	
+	this.bodyNode = bodyNode;
+}
+
+NextImageEvent.prototype.action = function(){
+	this.modal.empty();
+	this.modal.append(this.bodyNode);
+}
+
 function ModalEvent(modal){
 	this.modal = modal;
 	var me = this;
@@ -56,26 +104,22 @@ function ModalEvent(modal){
 	}
 }	
 
-function TourGuide(map){
+function TourGuide(map, events){
 	this.map = map;
-	this.events = [];
-	this.currentEvent = -1;
-}
-
-TourGuide.prototype.addEvent =  function(latLng){
-	this.events.push(latLng);
+	this.events = events;
+	this.currentEvent = 0;
 }
 
 TourGuide.prototype.nextEvent = function(){
-	this.currentEvent ++;	
 	var ce = this.events[this.currentEvent];	
-	ce.action();	
+	if(ce.action() == "done"){
+		this.currentEvent ++;	
+	};	
 }
 
 function addKeyPressListener(){
 	$(document).keypress(function(event){
 		tourGuide.nextEvent();
-		window.console.log(event);
 	})
 }
 
@@ -86,10 +130,7 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
-	tourGuide = new TourGuide(map);	
-	for (var i in events){
-		tourGuide.addEvent(events[i]);
-	}
+	tourGuide = new TourGuide(map, events);	
 	tourGuide.nextEvent();
 	addKeyPressListener();
 }
